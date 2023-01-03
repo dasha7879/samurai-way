94e957940694c65704e2d70c5272123f4f5737cf
dasha7879 <belka7879@gmail.com>
2023-01-03 23:01:58 +0300

    feat: add folder components with files

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.shutterstock.com/image-vector/mountain-vector-icon-isolated-on-260nw-1199231911.jpg" alt="logo" />
+        </header>
+    )
+}
+export default Header;
